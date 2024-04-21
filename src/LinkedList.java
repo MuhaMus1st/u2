1e65a442ac20314667ad9097d9caad8cf1019d8e
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-21 23:59:55 +0500

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,43 @@
-public class LinkedList {
+public class MyLinkedList {
+
+
+    private MyNode head ;
+    private MyNode tail ;
+    private int size ;
+    public MyLinkedList() {
+        head = null ;
+        size = 0 ;
+    }
+
+    public void add(int newItem) {
+        MyNode newNode = new MyNode(newItem) ;
+        if (head == null) {
+            head = tail = newNode ;
+        }
+        else {
+            tail.next = newNode ;
+            tail = newNode ;
+        }
+
+        size++ ;
+    }
+
+    public int get (int index) {
+        MyNode current = head ;
+        for (int i = 0 ; i < index ; i++) {
+            current = current.next ;
+        }
+        return current.data ;
+    }
+
+    private static class MyNode {
+        int data ;
+        MyNode next ;
+
+        MyNode(int data) {
+            this.data = data ;
+            next = null ;
+        }
+    }
+
 }
